8df912b786424db4eb5f092bbe810a384233306f
Pulgarcit0 <104662717+Pulgarcit0@users.noreply.github.com>
2022-11-18 00:24:32 -0600

    Update Main.kt
    Se añadio comentarios

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane
 
 
 fun main(args: Array<String>) {
+        //Aqui se le pide los datos al usuario
         var h=Integer.parseInt(JOptionPane.showInputDialog(null,"Hora: "))
         var m=Integer.parseInt(JOptionPane.showInputDialog(null,"Minuto: "))
         var s=Integer.parseInt(JOptionPane.showInputDialog(null,"Segundo: "))
@@ -15,11 +16,12 @@ fun main(args: Array<String>) {
         num=h*3600
         //OPERACION MINUTO A SEGUNDO
         num=num+(m*60)
-        //
+        //SE CALCULO DE LAS HORAS, MINUTOS Y SEGUNDO
         num +=s
         hor = num / 3600
         min = (num - 3600 * hor) / 60
         seg = num - (hor * 3600 + min * 60)
-        //println(hor.toString() + "h " + min + "m " + seg + "s")
+        //SE MANDA A IMPRIMIR EN PANTALL LA INFORMACION
         JOptionPane.showMessageDialog(null,"${hor.toString()}h $min m $seg g")
     }
+//by Hufolino Valentin @Pulgarcito Valentin
