99a00ee5c6a641edffe10ac07bf363357d773e28
valem <vale.metal520.69@gmail.com>
2022-11-18 14:39:04 -0600

    Se agregaron nuevas clases,y la funcion burbuja

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane
 
 
 fun main(args: Array<String>) {
+        //ESTAS TRES JOPTION, SON LOS QUE INTERACTUAN CON EL USUARIO, Y PIDEN DATOS
         var h=Integer.parseInt(JOptionPane.showInputDialog(null,"Hora: "))
         var m=Integer.parseInt(JOptionPane.showInputDialog(null,"Minuto: "))
         var s=Integer.parseInt(JOptionPane.showInputDialog(null,"Segundo: "))
@@ -11,15 +12,19 @@ fun main(args: Array<String>) {
         var min: Int
         var seg: Int
 
-        //OPERACION HORA A SEGUNDOS
+        //OPERACION HORA A SEGUNDOS, POR LO QUE SE GUARDA EN LA VARIABLE NUM
         num=h*3600
         //OPERACION MINUTO A SEGUNDO
         num=num+(m*60)
-        //
-        num +=s
+
+        //HACE LAS OPERACIONES NESESARIAS
+        num +=s //LA VARIABLE NUM, CONTIENE TODO LAS SUMA DE SEGUNDO, TANTO DE HORAS Y MINUTOS
         hor = num / 3600
         min = (num - 3600 * hor) / 60
         seg = num - (hor * 3600 + min * 60)
-        //println(hor.toString() + "h " + min + "m " + seg + "s")
+
+        //AQUE SE IMPRIME LOS DATOS YA GENERADOS
         JOptionPane.showMessageDialog(null,"${hor.toString()}h $min m $seg g")
     }
+// BY HUGOLINO VALENTI MARTINEZ
+// BY Pulgarcito Valentin
