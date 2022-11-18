87a7258a4e90438fc5c7b412d22a0166e79c9228
valem <vale.metal520.69@gmail.com>
2022-11-18 15:29:38 -0600

    F[2]

--- src/main/kotlin/NumRomanos.kt
+++ src/main/kotlin/NumRomanos.kt
@@ -6,3 +6,35 @@ fun main(){
 
 
 }
+fun roms(){
+    int x, 17
+    int Vn[]=( 1000, 900, 500, 400, 100, 90, 50, 40,10, 9, 5, 4
+    String Ve[]=("m", "cm", "a", "ca", "eo", "x", "1", "XL", "x", MAN", MVM, NAV, MAM)
+
+
+
+
+
+    while ( true )(
+            System.out.printin("\nFavor ingresar un numero decimal / 0 terminar: ")7
+
+    x = entrada.nextInt () 7
+
+    it (x ee 0)
+    break;
+
+
+
+    System.out.printin( x + "
+
+        1 = 03
+        while ( x>0 )(
+
+                it( x >= Vntil )¢
+    System.out.printin( Ve[i]):
+    x= x - Vali]:
+
+    yelse
+    feieias
+
+}
