9b19b233a48d290bfa2793737592f2f1d5f34377
Pulgarcit0 <vale.metal520.69@gmail.com>
2022-11-18 18:16:27 -0600

    Creacion de array y llenado[F3]

--- src/main/kotlin/NumRomanos.kt
+++ src/main/kotlin/NumRomanos.kt
@@ -1,40 +1,18 @@
+import javax.swing.JOptionPane
 
 fun main(){
 
-    var x=5
-    var r= arrayListOf<String>("m","cm","m","cm","m","cm","m","cm","m","cm","m","","IX","V","IV","I",)
+    var x:Int
+    var i:Int
+    var vn= arrayListOf<Int>(   1000, 900, 500, 400, 100,  90,  50,  40  ,10,   9,   5,   4,    1)
+    var ve= arrayListOf<String>( " M", "CM"," D"," CD"," C"," XC","I ","XI ","X","IX","V","IV","I")
+    var n=Integer.parseInt(JOptionPane.showInputDialog("Numero: "))
+    for (i in 1..n){
+        while (n>=vn[i]){
+
+        }
 
 
-}
-fun roms(){
-    int x, 17
-    int Vn[]=( 1000, 900, 500, 400, 100, 90, 50, 40,10, 9, 5, 4
-    String Ve[]=("m", "cm", "a", "ca", "eo", "x", "1", "XL", "x", MAN", MVM, NAV, MAM)
-
-
-
-
-
-    while ( true )(
-            System.out.printin("\nFavor ingresar un numero decimal / 0 terminar: ")7
-
-    x = entrada.nextInt () 7
-
-    it (x ee 0)
-    break;
-
-
-
-    System.out.printin( x + "
-
-        1 = 03
-        while ( x>0 )(
-
-                it( x >= Vntil )¢
-    System.out.printin( Ve[i]):
-    x= x - Vali]:
-
-    yelse
-    feieias
+    }
 
 }
