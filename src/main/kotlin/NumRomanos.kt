f13f9c6b2115ed4056de37b9885726b6d4690908
Hugolino Valentin <104662717+Pulgarcit0@users.noreply.github.com>
2022-11-18 22:32:25 -0600

    Update NumRomanos.kt

--- src/main/kotlin/NumRomanos.kt
+++ src/main/kotlin/NumRomanos.kt
@@ -2,17 +2,24 @@ import javax.swing.JOptionPane
 
 fun main(){
 
-    var x:Int
-    var i:Int
-    var vn= arrayListOf<Int>(   1000, 900, 500, 400, 100,  90,  50,  40  ,10,   9,   5,   4,    1)
-    var ve= arrayListOf<String>( " M", "CM"," D"," CD"," C"," XC","I ","XI ","X","IX","V","IV","I")
-    var n=Integer.parseInt(JOptionPane.showInputDialog("Numero: "))
-    for (i in 1..n){
-        while (n>=vn[i]){
+    var rom=12
+    var r= arrayListOf<String>("I","II","III","IV","V","VI","VIII","VII","IX","X")
+    var n= arrayListOf<Int>(1,2,3,4,5,6,7,8,9,10)
+    //var rom=Integer.parseInt(JOptionPane.showInputDialog("Numero"))
 
+    var re= arrayListOf<String>()
+    var tem= arrayListOf<String>()
+    for (i in 0 until 10) {
+
+        if (rom == n[i]) {
+            System.out.println("Posición: "+(i+1)+"-> "+r[i])
+        }else if (rom != n[i]){
+            for (j in 0..rom){
+                if(rom==j){
+                    System.out.println("Posición: "+j+"->>"+r[9]+r[i])
+                }
+            }
         }
-
-
     }
 
 }
