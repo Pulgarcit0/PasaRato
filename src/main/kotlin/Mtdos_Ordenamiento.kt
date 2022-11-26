5f2bfc4fc045557adfdf2151aba9745824028bd3
Pulgarcit0 <vale.metal520.69@gmail.com>
2022-11-26 16:58:52 -0600

    Se añadio quicsort y shelsort

--- src/main/kotlin/Mtdos_Ordenamiento.kt
+++ src/main/kotlin/Mtdos_Ordenamiento.kt
@@ -1,10 +1,15 @@
 import java.util.Arrays
 import javax.swing.JOptionPane
 
-fun Bubble(a:String) {
-    var listaNumerosTexto=a.split(",".toRegex()).toTypedArray()
-    var cantidad=listaNumerosTexto.size
-    var listaNumeros=IntArray(cantidad)
+
+
+
+
+fun Bubble() {
+    val a=JOptionPane.showInputDialog("Ingrese los numero y separador por comas ")
+    val listaNumerosTexto=a.split(",".toRegex()).toTypedArray()
+    val cantidad=listaNumerosTexto.size
+    val listaNumeros=IntArray(cantidad)
     for (i in (0 until cantidad)){
         listaNumeros[i]=listaNumerosTexto[i].toInt()
     }
@@ -29,7 +34,64 @@ fun Bubble(a:String) {
     }
 }
 
+fun main(args: Array<String>) {
 
-fun main(){
-    Bubble(JOptionPane.showInputDialog("Ingrese los numero y separador por comas "))
+    val numeros = intArrayOf(1, 9, 23, 4, 55, 100, 1, 1, 23)
+    println("Antes de QS: " + Arrays.toString(numeros))
+    quicksort(numeros, 0, numeros.size - 1)
+    println("Después de QS: " + Arrays.toString(numeros))
+}
+
+private fun particion(arreglo: IntArray, izquierda: Int, derecha: Int): Int {
+    // Elegimos el pivote, es el primero
+    var izquierda = izquierda
+    var derecha = derecha
+    val pivote = arreglo[izquierda]
+    // Ciclo infinito
+    while (true) {
+        // Mientras cada elemento desde la izquierda esté en orden (sea menor que el
+        // pivote) continúa avanzando el índice
+        while (arreglo[izquierda] < pivote) {
+            izquierda++
+        }
+        // Mientras cada elemento desde la derecha esté en orden (sea mayor que el
+        // pivote) continúa disminuyendo el índice
+        while (arreglo[derecha] > pivote) {
+            derecha--
+        }
+        /*
+Si la izquierda es mayor o igual que la derecha significa que no
+necesitamos hacer ningún intercambio
+de variables, pues los elementos ya están en orden (al menos en esta
+iteración)
+*/if (izquierda >= derecha) {
+            // Indicar "en dónde nos quedamos" para poder dividir el arreglo de nuevo
+            // y ordenar los demás elementos
+            return derecha
+        } else { //Nota: yo sé que el else no hace falta por el return de arriba, pero así el algoritmo es más claro
+            /*
+  Si las variables quedaron "lejos" (es decir, la izquierda no superó ni
+  alcanzó a la derecha)
+  significa que se detuvieron porque encontraron un valor que no estaba
+  en orden, así que lo intercambiamos
+  */
+            val temporal = arreglo[izquierda]
+            arreglo[izquierda] = arreglo[derecha]
+            arreglo[derecha] = temporal
+            /*
+  Ya intercambiamos, pero seguimos avanzando los índices una vez más
+  */izquierda++
+            derecha--
+        }
+        // El while se repite hasta que izquierda >= derecha
+    }
+}
+
+// Divide y vencerás
+private fun quicksort(arreglo: IntArray, izquierda: Int, derecha: Int) {
+    if (izquierda < derecha) {
+        val indiceParticion = particion(arreglo, izquierda, derecha)
+        quicksort(arreglo, izquierda, indiceParticion)
+        quicksort(arreglo, indiceParticion + 1, derecha)
+    }
 }
---
+++ src/main/kotlin/QuicSort.kt
@@ -0,0 +1,48 @@
+
+
+//coralillo 951 398 73 36
+/* function to sort array using shellSort in kotin*/
+fun sort(arr: IntArray): Int {
+    val n = arr.size
+
+    // decide the gap, then reduce the gap
+    var gap = n / 2
+    while (gap > 0) {
+        // Do a gapped insertion sort for this gap size.
+        // The first gap elements a[0..gap-1] are already
+        // in gapped order keep adding one more element
+        // until the entire array is gap sorted
+        var i = gap
+        while (i < n) {
+            // add a[i] to the elements that
+            // have been gap sorted save a[i] in temp and make a hole at
+            // position i
+            val temp = arr[i]
+
+            // shift earlier gap-sorted elements up until
+            //the correct location for a[i] is found
+            var j = i
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap]
+                j -= gap
+            }
+
+            // put temp (the original a[i]) in its correct
+            // location
+            arr[j] = temp
+            i += 1
+        }
+        gap /= 2
+    }
+    return 0
+}
+fun main(args: Array<String>) {
+    val arr = intArrayOf(23, 12, 1, 8, 34, 54, 2, 3)
+    println("Array before sorting")
+    for( a in arr) print("$a  ")
+
+    sort(arr)
+
+    println("Array after sorting")
+    for( a in arr) print("$a  ")
+}
